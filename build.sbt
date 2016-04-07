eac7a87b66af8bb39cb332dbbbcf7d2535cc0c32
inakov <ivannakov@hotmail.com>
2016-04-07 12:02:56 +0300

    - Save the trained model to file - Created test app with model

--- build.sbt
+++ build.sbt
@@ -9,4 +9,6 @@ libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.2.2"
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.0"
 libraryDependencies += "org.apache.spark" %% "spark-mllib" % "1.6.0"
 libraryDependencies += "joda-time" % "joda-time" % "2.9.3"
-    
+libraryDependencies += "org.scalanlp" %% "breeze-viz" % "0.12"
+libraryDependencies += "org.scalanlp" %% "breeze-natives" % "0.12"
+libraryDependencies += "org.scalanlp" %% "breeze" % "0.12"
--- src/main/scala/MainWifi.scala
+++ src/main/scala/MainWifi.scala
@@ -16,7 +16,7 @@ object MainWifi extends App{
   val rawData = sc.textFile("/home/inakov/Documents/trace-v3-10feed900252-2016-04-05.csv")
   val data = rawData.map(line => line.split(",")).collect {
     case check => ((check(0).toDouble * 1000).toLong, check(2), check(3), check(4), check(5))
-  }.filter(_._5 == "STA").cache()
+  }.filter(_._5 == "STA").map(check => check.copy(_1 = check._1 - 1459803600000l)).cache()
 
 //  data.take(100).foreach(println)
 //  val countByUser = data.map(check => (check._2, 1)).reduceByKey(_ + _).sortBy(-_._2)
@@ -25,7 +25,7 @@ object MainWifi extends App{
 //  println(s"Number of phones with more then 5 checks ${countByUser.filter(_._2 > 5).count()}")
 //  countByUser.collect().foreach(println)
 
-  groupByUser.filter(_._2.size > 5).map(_._2.map(_._4.toInt)).map(mean(_)).filter(_ > -67).collect().foreach(println)
+  val sessions = groupByUser.filter(_._2.size > 5).sortBy(-_._2.size).filter(check => mean(check._2.map(_._4.toInt)) > -67).map(_._2.map(check => (check._1, check._4.toInt)))
 
   def mean[T](item:Traversable[T])(implicit n:Numeric[T]) = {
     n.toDouble(item.sum) / item.size.toDouble
@@ -37,4 +37,19 @@ object MainWifi extends App{
 //  println(summary.min)
 //  println(summary.count)
 //  println(summary.variance)
+
+  import breeze.linalg._
+  import breeze.plot._
+
+  val f = Figure()
+  val p = f.subplot(0)
+  println(sessions.count())
+  val firstSession = sessions.take(10).last
+  val x = new DenseVector(firstSession.map(_._1.toInt).toArray)
+  val y = new DenseVector(firstSession.map(_._2).toArray)
+  p += plot(x, y)
+  p.xlabel = "time of the day in millis"
+  p.ylabel = "signal"
+  f.saveas("lines.png")
+
 }
